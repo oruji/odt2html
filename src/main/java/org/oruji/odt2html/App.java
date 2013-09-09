bbcd8ad6bb84581fc95006bee9fa0d327e36c8c1
Amin Oruji <aminpy@aminpy.com>
2013-09-09 10:31:52 +0430

    access to paragraphs

--- src/main/java/org/oruji/odt2html/App.java
+++ src/main/java/org/oruji/odt2html/App.java
@@ -1,13 +1,34 @@
 package org.oruji.odt2html;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import java.io.File;
+import java.io.IOException;
+
+import org.jopendocument.dom.ODPackage;
+
+public class App {
+	public static void main(String[] args) throws IOException {
+		ODPackage p = new ODPackage(new File("/home/aminpy/durud.odt"));
+
+		for (int i = 0; i < p.getTextDocument().getParagraphCount(); i++) {
+			System.out.println(p.getTextDocument().getParagraph(i)
+					.getCharacterContent());
+		}
+
+		// System.out.println(p.getTextDocument().getParagraph(0)
+		// .getCharacterContent(true));
+
+		// System.out.println(p.getTextDocument().getCharacterContent(true));
+
+		// ODSingleXMLDocument doc = p.toSingle();
+
+		// final Heading heading = new Heading();
+		// heading.setStyle("Movie_20_Heading");
+		// heading.addContent("Here is my title");
+		// doc.add(heading);
+		//
+		// final Paragraph paragraph = new Paragraph();
+		// paragraph.setStyle("Synopsis_20_Para");
+		// paragraph.addContent("Here is my paragraph");
+		// doc.add(paragraph);
+	}
 }
