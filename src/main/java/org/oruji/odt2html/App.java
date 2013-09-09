1d7f0c907e31555f6e5df1da464f157ec387783d
Amin Oruji <aminpy@aminpy.com>
2013-09-09 10:24:25 +0430

    create maven

---
+++ src/main/java/org/oruji/odt2html/App.java
@@ -0,0 +1,13 @@
+package org.oruji.odt2html;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
